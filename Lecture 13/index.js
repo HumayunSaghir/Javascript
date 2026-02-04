5a9f3969e331cb960af7fa2a22c43989391d0dc4
HumayunSaghir <hr3296726@gmail.com>
2026-02-04 22:51:22 +0500

    updated

---
+++ Lecture 13/index.js
@@ -0,0 +1,21 @@
+// let currentDate = new Date()
+// console.log(currentDate.toLocaleString())
+
+// let myDate = new Date(2002, 6, 23, 6, 50, 23)
+// console.log(myDate.toLocaleString())
+
+// let birthday = new Date(2002, 6, 23, 6, 50, 23)
+// let currentDate = new Date()
+
+// console.log(currentDate.getFullYear() - birthday.getFullYear())
+
+let date = new Date("7-23-2002")
+// console.log(date.toLocaleString())
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(`day was ${date.getDay()} and month was ${date.getMonth()}`)
+// console.log(date.toLocaleString('default', {
+    // weekday : "long",
+// }))
+// console.log(date.getTime())
+// console.log(Date.now())
