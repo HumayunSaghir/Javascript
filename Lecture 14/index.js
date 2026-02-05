75b40841852ab86d8ad4bb83e1f32eb7f8166e75
HumayunSaghir <hr3296726@gmail.com>
2026-02-05 23:49:54 +0500

    updated

---
+++ Lecture 14/index.js
@@ -0,0 +1,38 @@
+// let arr = [1, 2, 3, 4, 5, "postman", true]
+// console.log(arr)
+// console.log(typeof arr)
+// arr.push("Lockheed")
+// console.log(arr)
+
+// let nums1 = [1, 2, 3, 4, 5]
+// let nums2 = nums1
+// nums1.push(-1)
+// console.log(nums1)
+// console.log(nums2)
+
+let nums1 = new Array(1, 2, 3, 4, 5, "Alexa")
+// console.log(nums1)
+// console.log(typeof nums1)
+
+// console.log(nums1.length)
+// nums1.pop()
+// console.log(nums1.length)
+// nums1.unshift(100)
+// nums1.shift()
+// console.log(nums1)
+// console.log(nums1.includes("Alexa"))
+// console.log(nums1.indexOf("Alexa"))
+
+// let string2 = nums1.join()
+// console.log(string2)
+// console.log(typeof string2)
+
+// let nums2 = [1, 2, 3, 4, 5]
+// let nums3 = nums2.splice(0, 2)
+// console.log(nums2)
+// console.log(nums3)
+
+// let nums2 = [1, 2, 3, 4, 5]
+// let nums3 = nums2.splice(0, 2)
+// console.log(nums2)
+// console.log(nums3)
