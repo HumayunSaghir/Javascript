544363b08ab3e930baca6a543e4f4a66178f77e8
HumayunSaghir <hr3296726@gmail.com>
2026-02-09 22:14:37 +0500

    updated

---
+++ Lecture 18/index.js
@@ -0,0 +1,10 @@
+// let userInfo = {
+//     userName : "Humayun Saghir",
+//     userAge : 23,
+//     userEmail : "abc@gmail.com",
+// }
+
+// console.log(userInfo.userEmail)
+// const {userEmail : email} = userInfo
+// console.log(email)
+
