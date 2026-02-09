6c6566851a407eb0446f1c12dd2aa0766ab10f7a
HumayunSaghir <hr3296726@gmail.com>
2026-02-09 22:32:16 +0500

    updated

---
+++ Lecture 20/index.js
@@ -0,0 +1,44 @@
+// function check(num1){
+//     return num1;
+// }
+
+// console.log(check(10))
+
+// function checkOne(...nums){
+//     return nums
+// }
+
+// const arr = checkOne(10, 20, 30, 40, 50)
+
+// let sum = 0
+
+// for(let i = 0; i < arr.length; i++){
+//     sum += arr[i]
+// }
+
+// console.log(sum)
+
+// let userInfo = {
+//     userName : "aaxe",
+//     userAge : 23,
+// }
+
+// function check(obj){
+//     console.log(obj.userName)
+// }
+
+// check(userInfo)
+
+// let arr = [1, 2, 3, 4, 5]
+
+// function sum(arr){
+//     let sum = 0
+
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i]
+//     }
+
+//     return sum
+// }
+
+// console.log(sum(arr))
