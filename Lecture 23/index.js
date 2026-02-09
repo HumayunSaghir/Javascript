b741d283dcf1e9e4098e289cb1ea0e16b1e3b22b
HumayunSaghir <hr3296726@gmail.com>
2026-02-10 00:12:43 +0500

    updated

---
+++ Lecture 23/index.js
@@ -0,0 +1,25 @@
+// "this" -> refers to the object that is calling the function.
+
+// let userInfo = {
+//     userName : "aaxe",
+//     greetings : function(){
+//         console.log(this)
+//     }
+// }
+
+// userInfo.greetings()
+// function greetings(){
+//     let username = "aaxe"
+//     console.log(this.username)
+// }
+
+// greetings()
+
+// let greetings = () => {
+//     console.log(this)
+// }
+
+// greetings()
+
+// let obj = () => ({userName : "aaxe"})
+// console.log(obj())
