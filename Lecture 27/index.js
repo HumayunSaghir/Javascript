d72143bbf607fca25ed678f5b16f34035f5f0edb
HumayunSaghir <hr3296726@gmail.com>
2026-02-11 00:59:02 +0500

    updated

---
+++ Lecture 27/index.js
@@ -0,0 +1,35 @@
+let arr = [1, 2, 3, 4, 5]
+
+for(let i = 0; i < arr.length; i++){
+
+    if(arr[i] === 3){
+        continue
+    }
+
+    console.log(arr[i])
+}
+
+// let i = 0;
+
+// while(i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+// let i = 0
+
+// do{
+//     console.log(i++)
+// }while(i <= 10)
+
+// let arr = [1, 2, 3, 4, 5]
+
+// arr.forEach(element => {
+    
+//     if(element === 3){
+//         return
+//     }
+
+//     console.log(element)
+// })
+
