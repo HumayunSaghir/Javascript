db13e9c73eb6818760f07ea1ac771112242ed45d
HumayunSaghir <hr3296726@gmail.com>
2026-02-11 00:51:43 +0500

    updated

---
+++ Lecture 26/index.js
@@ -0,0 +1,72 @@
+// if("2" == 2){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+// if(2 > 1) console.log("2 is greater than 1")
+
+// if(1 > 1){
+//     console.log("entered if block!")
+// }
+// else if(2 >= 2){
+//     console.log("entered else if block")
+// }
+// else{
+//     console.log("entered else block")
+// }
+
+// if(true || false){
+//     console.log("entered if block")
+// }
+
+// let number = 1
+
+// switch(number){
+//     case 1:
+//         console.log("Monday")
+//         break
+//     case 2:
+//         console.log("Tuesday")
+//         break
+//     case 3:
+//         console.log("Wednesday")
+//         break
+//     case 4:
+//         console.log("Thursday")
+//         break
+//     case 5:
+//         console.log("Friday")
+//         break
+//     case 6:
+//         console.log("Saturday")
+//         break
+//     case 7:
+//         console.log("Sunday")
+//         break
+//     default:
+//         console.log("Invalid Input")
+//         break
+// }
+
+// console.log(Boolean(""))
+// let arr = []
+// console.log(Boolean(arr))
+
+let obj = {}
+
+function greet(){}
+
+// console.log(Boolean(greet))
+// console.log(Object.keys(obj).length)
+
+// let value = null ?? undefined ?? 10
+// console.log(value)
+
+// (2 > 1) ? console.log("correct") : console.log("incorrect")
+
+let value = null ?? undefined ?? 10
+console.log(value)
+
+(2 > 1) ? console.log("correct") : console.log("incorrect")
