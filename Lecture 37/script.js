54fc42504b5f3e465825e31759ddfa8a3b5d9b82
HumayunSaghir <hr3296726@gmail.com>
2026-02-20 01:04:06 +0500

    updated

---
+++ Lecture 37/script.js
@@ -0,0 +1,23 @@
+// setInterval(()=>{
+//     console.log("Hello World")
+// }, 2000)
+
+// setTimeout(()=>{
+//     console.log("one")
+// }, 1)
+
+// setTimeout(()=>{
+//     console.log("two")
+// }, 0)
+
+// setTimeout(()=>{
+//     console.log("three")
+// }, 1)
+
+console.log("One")
+
+setTimeout(() => {
+    console.log("Two")
+}, 0)
+
+console.log("Three")
