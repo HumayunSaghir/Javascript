7a5cfe98777aea600938296b85a843f843362581
HumayunSaghir <hr3296726@gmail.com>
2026-03-12 23:01:36 +0500

    updated

---
+++ Lecture 44/index.js
@@ -0,0 +1,35 @@
+// let userInfo = {
+//     userName : "aaxe",
+//     userAge : 22,
+//     userEmail : "abc@gmail.com",
+//     getInfo : function(){
+//         console.log(this)
+//     }
+// }
+// userInfo.getInfo()
+
+// function greet(){
+//     console.log(this)
+// }
+// greet()
+
+// function setUsername(username){
+//     this.username = username
+//     return this
+// }
+
+// const userOne = new setUsername("aaxe")
+// console.log(userOne.username)
+
+function setUsername(username){
+    this.username = username
+}
+
+function createUser(username, email, age){
+    setUsername.call(this, username)
+    this.email = email
+    this.age = age
+}
+
+const userOne = new createUser("aaxe", "abc@gmail.com", 22)
+console.log(userOne)
